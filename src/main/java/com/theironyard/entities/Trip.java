5657fcc07ad9d72bc8861bc239db5daa05b3d769
Ser-Castic <samsteinberg90@gmail.com>
2017-02-08 14:21:52 -0500

    Added some html for testing purposes

--- src/main/java/com/theironyard/entities/Trip.java
+++ src/main/java/com/theironyard/entities/Trip.java
@@ -20,4 +20,49 @@ public class Trip {
 
     @ManyToOne
     User user;
+
+    public Trip(String tripName, String startLocation, String endLocation, User user) {
+        this.tripName = tripName;
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.user = user;
+    }
+
+    public Trip(String tripName, String startLocation, String endLocation) {
+        this.tripName = tripName;
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+    }
+
+    public String getTripName() {
+        return tripName;
+    }
+
+    public void setTripName(String tripName) {
+        this.tripName = tripName;
+    }
+
+    public String getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getEndLocation() {
+        return endLocation;
+    }
+
+    public void setEndLocation(String endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
